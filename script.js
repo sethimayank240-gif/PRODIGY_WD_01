1ad89dcb16c22369ecce8efd0fb1e88f6666f564
sethimayank240-gif <sethimayank240@gmail.com>
2025-07-31 21:24:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  const nav = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-});
